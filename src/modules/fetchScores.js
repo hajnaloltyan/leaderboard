16011a292203c6557b1c256c5d62fd7d49078df9
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-06-21 23:35:50 +0200

    Sort scores, adjust layout of main elements

--- src/modules/fetchScores.js
+++ src/modules/fetchScores.js
@@ -3,7 +3,7 @@ import { getScores } from './handleAPI.js';
 const fetchScores = async () => {
   const data = await getScores();
   const dataResult = await data.result;
-  return dataResult;
+  return dataResult.sort((a, b) => b.score - a.score);
 };
 
 export default fetchScores;
